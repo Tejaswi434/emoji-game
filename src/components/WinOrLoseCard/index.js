2d490e81f9760f751e34d3c19200a587456ef17a
Tejaswi434 <141810258+Tejaswi434@users.noreply.github.com>
2024-01-15 08:22:28 +0530

    Update index.js

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,1 +1,26 @@
 // Write your code here.
+import './index.css'
+
+const win = 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
+const loose = 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
+const WinorLose = props => {
+  const {isWon, score, onClickPlayAgain} = props
+  const imageUrl = isWon ? win : loose
+  const gameStatus = isWon ? 'You Won' : 'You loose'
+  const bestScore = isWon ? 'BestScore' : 'Score'
+  return (
+    <div>
+      <div>
+        <h1>{gameStatus}</h1>
+        <p>{bestScore}</p>
+        <p>{gameStatus}</p>
+        <button onClick={onClickPlayAgain}>playAgain</button>
+      </div>
+      <div>
+        <img src={imageUrl} />
+      </div>
+    </div>
+  )
+}
+
+export default WinorLose
