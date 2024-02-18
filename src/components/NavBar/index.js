0ef15cb3c6214e65a667ac14606252d908a60f69
Tejaswi434 <141810258+Tejaswi434@users.noreply.github.com>
2024-02-18 08:11:40 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,24 +2,30 @@
 
 import './index.css'
 
-const NavBar = props => {
-  const {topScore, currentScore} = props
-  return (
-    <div className="emojiLogo">
-      <div className="rowing">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-          alt="emoji logo"
-        />
-        <h1 className="whiteColor">Emoji Game</h1>
-      </div>
+const win = 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
+const loose = 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
+const WinorLose = props => {
+  const {isWon, score, onClickPlayAgain} = props
+  const imageUrl = isWon ? win : loose
+  const gameStatus = isWon ? 'You Won' : 'You Lose'
+  const bestScore = isWon ? 'Best Score' : 'Score'
 
-      <div className="rowing">
-        <p className="score">Score :{currentScore} </p>
-        <p className="topScore">TopScore:{topScore}</p>
+  return (
+    <div className="back_1">
+      <div className="back_2">
+        <h1>{gameStatus}</h1>
+        <p className="scoreColor">{bestScore}</p>
+
+        <p className="number">{score}/12</p>
+        <button onClick={onClickPlayAgain} className="yellow" type="button">
+          Play Again
+        </button>
+      </div>
+      <div>
+        <img src={imageUrl} alt="win or lose" />
       </div>
     </div>
   )
 }
 
-export default NavBar
+export default WinorLose
